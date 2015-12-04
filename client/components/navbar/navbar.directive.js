f50301d38e50c09880a983f5b817f046642a4b6e
Devin Miller <edwin0258@gmail.com>
2015-12-04 01:15:55 +0000

    Initial Commit

---
+++ client/components/navbar/navbar.directive.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('workspaceApp')
+  .directive('navbar', function () {
+    return {
+      templateUrl: 'components/navbar/navbar.html',
+      restrict: 'E',
+      controller: 'NavbarCtrl'
+    };
+  });
